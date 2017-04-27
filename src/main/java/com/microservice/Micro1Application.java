196324599294d5628070ed1376d5db50f7744d96
dancb <dan.cbaez@gmail.com>
2017-04-27 08:51:42 -0300

    comment 1

--- src/main/java/com/microservice/Micro1Application.java
+++ src/main/java/com/microservice/Micro1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Micro1Application {
 
+	// simple comment
 	public static void main(String[] args) {
 		SpringApplication.run(Micro1Application.class, args);
 	}
